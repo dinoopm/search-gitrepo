8ad2a34688d38fac250817775dcfd7215936281e
Dinoop <dinoopm@gmail.com>
2018-08-23 10:34:10 +0200

    - API doc comment moved to controller

--- server/src/api/controllers/gitrepo.controller.js
+++ server/src/api/controllers/gitrepo.controller.js
@@ -1,10 +1,27 @@
 const services = require('../services/gitrepo')
 
 /**
- * Search github based on query
- * Returns repositories
- * @public
+ * @api {post} gitrepo/search-repositories/:q Search and retrieve repositories 
+ * @apiName searchRepositories
+ * @apiGroup Search
+ * @apiVersion 0.1.0
+ *
+ * @apiParam {String} id search text
+ *
+ * @apiSuccess {Array} List of repositories based on search text.
+ * @apiSuccess {Number} count  Total number of repositories based on search text.
+ *
+ * @apiSuccessExample Success-Response:
+ *     
+ *     {
+ *       "count": 1,
+ *       "list": [{}]
+ *     }
+ *
+ *
  */
+
+
 exports.searchRepositories = async (req, res, next) => {
   try { 
 
--- server/src/api/services/gitRepo.js
+++ server/src/api/services/gitRepo.js
@@ -1,31 +1,6 @@
  /* eslint-disable camelcase */
 const axios = require('axios');
 
-
-
-/**
- * @api {post} gitrepo/search-repositories/:q Search and retrieve repositories 
- * @apiName searchRepositories
- * @apiGroup Search
- * @apiVersion 0.1.0
- *
- * @apiParam {String} id search text
- *
- * @apiSuccess {Array} List of repositories based on search text.
- * @apiSuccess {Number} count  Total number of repositories based on search text.
- *
- * @apiSuccessExample Success-Response:
- *     
- *     {
- *       "count": 1,
- *       "list": [{}]
- *     }
- *
- *
- */
-
-
-
 exports.searchRepositories = async (q) => {
   
   const url = `https://api.github.com/search/repositories?q=${q}`;
